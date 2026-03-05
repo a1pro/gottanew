80c3ce1e9160556c28cf70a26c1ee49b1a71bbdd
Eweb a1professionals <teamlead@a1professionals.com>
2026-03-05 13:18:56 +0530

    update api php file
    update api php file

--- routes/api.php
+++ routes/api.php
@@ -8,10 +8,9 @@ Route::prefix('v1')->group(function () {
     
     // Include all route files
     require __DIR__ . '/api/auth.php';
-    require __DIR__ . '/api/profile.php';
-    require __DIR__ . '/api/sessions.php';
-    require __DIR__ . '/api/coaches.php';
     require __DIR__ . '/api/coach.php';
     require __DIR__ . '/api/client.php';
     require __DIR__ . '/api/admin.php';
+    require __DIR__ . '/api/analytics.php';
+    require __DIR__ . '/api/finance.php';
 });
